93c6acc1f23b7ff557a413698979f336ac57227c
Arjina <31024242+ArjinaKhanom@users.noreply.github.com>
2025-04-13 16:16:41 -0400

    javadoc is done for all the levels. Project 2 is done.

--- src/ListLinksLevel5.java
+++ src/ListLinksLevel5.java
@@ -1,3 +1,20 @@
+/* Level 5 Goals – output graph and packaging 
+To be awarded full credit for Level 5, you must demonstrate the following: 
+• Outputting to the console the entire tree in DOT graph language format.  That is, one should 
+be able to execute a dot interpreter on the output and receive a visual. 
+o For each level print the node + label. 
+o Then, print that this node connects to a number of other nodes (names of children). 
+• You can test your program by copying the output into a GraphViz generator website. 
+• I will test with java -jar yourjar.jar | dot -Tsvg > result.svg.  You can view the command line 
+arguments here: https://graphviz.org/doc/info/command.html.  
+• Has appropriate JavaDoc documentation for all classes. 
+• Has all necessary source code. 
+• Has an executable Jar. 
+• Has screenshots demonstrating successful operation of Goal 1, 2, 3, and 4. 
+• Has a weekly log of accomplishments, with each group member appropriately attributed. 
+ */
+
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,12 +25,43 @@ import java.util.Set;
 
 /**
  * ListLinksLevel5 crawls a website and outputs its structure as a DOT graph.
- * Use Graphviz to visualize the output.
+ * <p>
+ * This program starts at a given URL, extracts all the links recursively up to a specified depth,
+ * and outputs the results in DOT format. The generated DOT format can be visualized using Graphviz or
+ * other compatible visualization tools. The program outputs each node with its title and connects each
+ * node to its child nodes.
+ * </p>
+ * <p>
+ * For example, the output can be used to visualize website link structures as a graph. You can paste the
+ * output into an online Graphviz generator to view the resulting visualization.
+ * </p>
+ *
+ * <p>
+ * <b>Usage:</b><br>
+ * To generate the DOT file, run the following command:<br>
+ * <code>java -cp ".;..\libs\jsoup-1.19.1.jar;ListLinksLevel5.jar" ListLinksLevel5 > output.dot</code>
+ * </p>
+ *
+ * @see <a href="https://dreampuf.github.io/GraphvizOnline/">Graphviz Online</a>
+ * @author Elizabeth
+ * @author Arjina
  */
+
+ //ListLinksLevel5 crawls a website and outputs its structure as a DOT graph.
+ // Use Graphviz to visualize the output.
 public class ListLinksLevel5 {
     private static final int MAX_DEPTH = 2;  // Adjust depth for testing
     private static Set<String> visited = new HashSet<>();
 
+    /**
+     * The entry point of the program. Fetches the given URL, builds the link tree, and outputs it in DOT format.
+     * <p>
+     * This method fetches the starting URL, constructs a tree of links up to the maximum depth, and prints
+     * the tree in DOT graph format for visualization.
+     * </p>
+     * 
+     * @param args Command-line arguments (unused)
+     */
     public static void main(String[] args) {
         String url = "https://www.hunter.cuny.edu";
         System.out.println("Fetching " + url + "...");
@@ -29,6 +77,17 @@ public class ListLinksLevel5 {
         }
     }
 
+     /**
+     * Builds a tree of links starting from the given URL up to a specified depth.
+     * <p>
+     * This method crawls a given URL, recursively fetches linked pages, and builds a tree of nodes. The tree's
+     * depth is limited by the {@link #MAX_DEPTH} constant.
+     * </p>
+     * 
+     * @param url The starting URL to crawl
+     * @param depth The current depth in the tree (0 for the root)
+     * @return The root node of the tree, or null if the page could not be fetched
+     */
     private static TreeNodeLevel5 buildLinkTree(String url, int depth) {
         if (depth > MAX_DEPTH || visited.contains(url)) {
             return null;
@@ -60,6 +119,15 @@ public class ListLinksLevel5 {
         }
     }
 
+    /**
+     * Recursively prints the DOT graph representation of the tree.
+     * <p>
+     * This method outputs each node in the tree along with its label and the connections between the nodes
+     * in DOT format. Each node represents a webpage, and edges represent links between those pages.
+     * </p>
+     *
+     * @param node The current node to print in DOT format
+     */
     private static void printDotGraph(TreeNodeLevel5 node) {
         String nodeName = "\"" + node.getUrl() + "\"";
         String nodeLabel = "[label=\"Level " + node.getDepth() + ": " + node.getTitle() + "\"]";
@@ -89,3 +157,4 @@ public class ListLinksLevel5 {
 
 // To create dot file:  java -cp ".;..\libs\jsoup-1.19.1.jar;ListLinksLevel5.jar" ListLinksLevel5 > output.dot
 // This above command line helps to create a create a file named output.dot containing the DOT graph
+// to run javadoc: javadoc -d javadoc -cp ".;..\libs\jsoup-1.19.1.jar" ListLinksLevel5.java
--- src/TreeNodeLevel5.java
+++ src/TreeNodeLevel5.java
@@ -4,6 +4,8 @@ import java.util.List;
 /**
  * TreeNodeLevel5 represents a node in the webpage link tree,
  * extended for Level 5 with depth tracking for DOT graph output.
+ * Each node contains information about a URL, its title, and its depth in the tree.
+ * It also stores a list of child nodes, allowing for the creation of a hierarchical structure.
  */
 public class TreeNodeLevel5 {
     private String url;
@@ -11,6 +13,13 @@ public class TreeNodeLevel5 {
     private int depth;
     private List<TreeNodeLevel5> children;
 
+    /**
+     * Constructs a TreeNodeLevel5 with the specified URL, title, and depth.
+     *
+     * @param url   the URL associated with this node
+     * @param title the title of the webpage
+     * @param depth the depth level of this node in the tree
+     */
     public TreeNodeLevel5(String url, String title, int depth) {
         this.url = url;
         this.title = title;
@@ -18,23 +27,50 @@ public class TreeNodeLevel5 {
         this.children = new ArrayList<>();
     }
 
+    /**
+     * Adds a child node to this tree node.
+     *
+     * @param child the child TreeNodeLevel5 to be added
+     */
     public void addChild(TreeNodeLevel5 child) {
         children.add(child);
     }
 
+    /**
+     * Returns the list of child nodes for this tree node.
+     *
+     * @return a List of TreeNodeLevel5 representing the children
+     */
     public List<TreeNodeLevel5> getChildren() {
         return children;
     }
 
+     /**
+     * Returns the URL associated with this tree node.
+     *
+     * @return the URL of this tree node
+     */
     public String getUrl() {
         return url;
     }
 
+    /**
+     * Returns the title of the webpage for this tree node.
+     *
+     * @return the title of the webpage
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Returns the depth level of this tree node in the tree structure.
+     *
+     * @return the depth of this node
+     */
     public int getDepth() {
         return depth;
     }
 }
+// to generate javadoc
+//javadoc -d javadoc -cp ".;..\libs\jsoup-1.19.1.jar" TreeNodeLevel5.java
